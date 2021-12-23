c3acdcf8770294da70257307ec2b7703535e2ed0
Felipe Ospina <felipeospina21@hotmail.com>
2021-12-23 15:22:15 -0500

    remove routes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import AboutPage from './pages/AboutPage/AboutPage';
-import ProductDetailPage from './pages/ProductDetailPage/ProductDetailPage';
 import Layout from './components/Layout/Layout';
-import ProductDetail from './components/ProductCard/ProductDetail';
+
 
 function App() {
   return (
@@ -12,9 +11,6 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='about' element={<AboutPage />} />
-        <Route path='detail' element={<ProductDetailPage />}>
-          <Route path=':id' element={<ProductDetail />} />
-        </Route>
       </Routes>
     </Layout>
   );
